aec8a013edf5f5f743fa60cc8d7f18eb9385adc5
kikugawa-shoma <59906819+kikugawa-shoma@users.noreply.github.com>
2020-11-22 09:46:33 +0900

    Kikugawa/feat/unittest (#4)
    * feat: initialize unittest
    
    * feat:unittest
    
    Co-authored-by: kikugawa-shoma <kikugawa.shukatsu@gmail.com>

---
+++ fizzbuzz/tests/test_fizzbuzz.py
@@ -0,0 +1,16 @@
+# 実行コマンド
+# python -m unittest
+from unittest import TestCase
+from ..fizzbuzz.fizzbuzz import fizzbuzz
+
+
+class TestFizzBuzz(TestCase):
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(3), "Fizz")
+        self.assertEqual(fizzbuzz(5), "Buzz")
+        self.assertEqual(fizzbuzz(15), "FizzBuzz")
+        self.assertEqual(fizzbuzz(2), "2")
+
+
+if __name__ == "__main__":
+    TestFizzBuzz().test_fizzbuzz()
